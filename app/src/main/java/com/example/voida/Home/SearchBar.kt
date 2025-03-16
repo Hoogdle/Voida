dc90bf54c07794774a6df4aca6ca21131c59ea4a
Kim Taeyeong <xodud7737@gmail.com>
2025-03-16 21:37:19 +0900

    _

--- app/src/main/java/com/example/voida/Home/SearchBar.kt
+++ app/src/main/java/com/example/voida/Home/SearchBar.kt
@@ -1,57 +1,290 @@
 package com.example.voida.Home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.voida.R
 import com.example.voida.ui.theme.DefaultSearchBar
 
+import android.Manifest
+import android.R.attr.singleLine
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.Locale
+
+
+
+// TODO
+// 1. make voice-search button
+// 2. complete voice-search function
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(){
 
-    var input by remember{mutableStateOf("")}
+    val context = LocalContext.current
+    val input = rememberTextFieldState()
+    val interactionSource = remember { MutableInteractionSource() }
     Row(
         modifier = Modifier
-            .padding(10.dp)
+            .padding(
+                horizontal = 10.dp,
+                vertical = 5.dp
+            )
     ){
         BasicTextField(
-            state = TextFieldState(initialText = "검색"),
+            state = input,
             textStyle = TextStyle(
                 fontFamily = FontFamily(Font(R.font.inter_18_bold)),
-                fontSize = 15.sp
+                fontSize = 15.sp,
             ),
-            decorator = {innerTextField ->
+            decorator = @Composable{innerTextField ->
+
                 Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
-                        .height(30.dp)
+                        .height(45.dp)
                         .width(250.dp)
                         .background(DefaultSearchBar)
+                        .padding(10.dp)
+
                 ){
-                    painterResource(R.dr)
-                    innerTextField()
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.search),
+                        contentDescription = "검색버튼",
+                        tint = Color(0x82828282)
+                    )
+                    Spacer(modifier = Modifier
+                        .width(5.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .border(
+                                width = 1.dp,
+                                color = DefaultSearchBar,
+                                shape = RoundedCornerShape(4.dp)
+                            )
+                    ){
+                        if(input.text.isEmpty()){
+                            Text(
+                                text = "검색",
+                                color = Color(0x82828282)
+                            )
+                        }
+                        Column {
+                            Spacer(modifier = Modifier.size(2.dp))
+                            innerTextField()
+                        }
+                    }
+
+                    Button(
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonColors(
+                            containerColor = DefaultSearchBar,
+                            contentColor = Color.Black,
+                            disabledContentColor = Color.Black,
+                            disabledContainerColor = DefaultSearchBar
+                        ),
+                        onClick = {},
+
+                        ){
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.search),
+                            contentDescription = "검색버튼",
+                        )
+                    }
+
                 }
             }
         )
+
+    }
+}
+
+@Composable
+fun SearchwithVocie(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var prompt by remember { mutableStateOf("") }
+
+    // Launcher for speech recognition
+    val speechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = { result ->
+            val spokenText =
+                result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.firstOrNull()
+            if (spokenText != null) {
+                prompt = spokenText  // Update prompt with recognized text
+            } else {
+                Toast.makeText(context, "음성인식에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+
+            BasicTextField(
+                textStyle = TextStyle.Default.copy(fontSize = 40.sp),
+
+                value = prompt,
+                onValueChange = { prompt = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(
+                        vertical = 8.dp,
+                        horizontal = 2.dp)
+                    .background(Color.White)
+                    .height(70.dp),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    if (prompt.isEmpty()) {
+                        Text("상품검색",
+                            color = Color.Gray,
+                            fontFamily = FontFamily(Font(R.font.inter_18_bold   )),
+                            fontSize = 30.sp,
+                            modifier = Modifier.padding(15.dp))
+                    }
+                    innerTextField()
+                    Button(
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonColors(
+                            containerColor = DefaultSearchBar,
+                            contentColor = Color.Black,
+                            disabledContentColor = Color.Black,
+                            disabledContainerColor = DefaultSearchBar
+                        ),
+                        onClick = {},
+
+                        ){
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.search),
+                            contentDescription = "검색버튼",
+                        )
+                    }
+
+                }
+            )
+
+
+            // Add a button to start voice recognition
+            Button(
+                shape = RectangleShape,
+                onClick = {
+                    if (ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.RECORD_AUDIO
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                        intent.putExtra(
+                            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                        )
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "음성인식 중..")
+                        speechRecognizerLauncher.launch(intent)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            context as Activity,
+                            arrayOf(Manifest.permission.RECORD_AUDIO),
+                            100
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .width(100.dp)
+                    .height(70.dp),
+                colors = ButtonColors(
+                    containerColor = DefaultSearchBar,
+                    contentColor = Color.White,
+                    disabledContentColor = Color.White,
+                    disabledContainerColor = DefaultSearchBar)
+            ) {
+                Text(
+                    "음성검색",
+                    fontFamily = FontFamily(Font(R.font.inter_18_bold)),
+                    fontSize = 12.sp,
+                )
+            }
+        }
     }
 }
